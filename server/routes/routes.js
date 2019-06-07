783f03616c632219598e580b37900de9becc42a3
Raymond Gakwaya <raymond42.gr@gmail.com>
2019-06-07 11:05:31 +0200

    [feature #166519895] add auhetications to a user posting a car add

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import signup from '../controllers/users/signup';
 import signin from '../controllers/users/signin';
 import Ads from '../controllers/cars/ads';
+import auth from '../middleware/auth';
 
 const router = express.Router();
 
@@ -12,7 +13,7 @@ router.post('/auth/signup', signup);
 router.post('/auth/signin', signin);
 
 // post a car ad
-router.post('/car', Ads);
+router.post('/car', auth, Ads);
 
 
 export default router;
