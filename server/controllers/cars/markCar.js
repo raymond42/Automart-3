06db0d2e86610b60a6f7ffa6ac54fe16095df9a4
Raymond Gakwaya <raymond42.gr@gmail.com>
2019-06-08 11:43:36 +0200

    [feature #166561159] add API for a user to mark a car posted ad as sold [finishes #166561159]

---
+++ server/controllers/cars/markCar.js
@@ -0,0 +1,65 @@
+import moment from 'moment';
+import validateUpdateStatus from '../../helpers/markCar';
+import pool from '../../config/db';
+
+const markadsold = async (req, res) => {
+  try {
+    const { error } = validateUpdateStatus.validation(req.body);
+    if (error) {
+      res.status(400).json({
+        status: 400,
+        error: error.details[0].message,
+      });
+      return;
+    }
+    const findCarId = 'SELECT * FROM cars WHERE id = $1';
+    const value = parseInt(req.params.id, 10);
+    const car = await pool.query(findCarId, [value]);
+
+    if (!car.rows[0]) {
+      res.status(404).json({
+        status: 404,
+        error: 'car post not found',
+      });
+    }
+    if (car.rows[0].status === req.body.status) {
+      res.status(400).json({
+        status: 400,
+        error: `The car is already marked as ${car.rows[0].status}`,
+      });
+    }
+    if (req.body.status === 'available') {
+      res.status(400).json({
+        status: 400,
+        error: 'you can not set the status of the car to available when it is sold!!',
+      });
+      return;
+    }
+
+    const carStatus = 'UPDATE cars SET status = $1 WHERE id = $2';
+    const values = [req.body.status, value];
+    await pool.query(carStatus, values);
+
+    const newCar = {
+      id: car.rows[0].id,
+      owner: car.rows[0].owner,
+      createdOn: moment().format('LL'),
+      manufacturer: car.rows[0].manufacturer,
+      model: car.rows[0].model,
+      price: car.rows[0].price,
+      state: car.rows[0].state,
+      status: req.body.status,
+    };
+
+    res.status(200).json({
+      status: 200,
+      data: newCar,
+    });
+    return;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+  }
+};
+
+export default markadsold;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,6 +5,7 @@ import Ads from '../controllers/cars/ads';
 import auth from '../middleware/auth';
 import Order from '../controllers/orders/order';
 import updatePriceOrder from '../controllers/orders/updatePriceOrder';
+import markadsold from '../controllers/cars/markCar';
 
 const router = express.Router();
 
@@ -23,5 +24,7 @@ router.post('/order', auth, Order);
 // updating the price of purchasing order
 router.patch('/order/:id/price', auth, updatePriceOrder);
 
+// mark a posted car ad as sold
+router.patch('/car/:id/status', auth, markadsold);
 
 export default router;
